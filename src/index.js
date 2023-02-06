61d65994fa9555eec43070614719ad2ae3bcd89a
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-02-06 22:54:52 +0700

    Basic Event

--- src/index.js
+++ src/index.js
@@ -42,12 +42,27 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author, children } = props;
+  const clickHandler = () => {
+    alert('hello');
+  }
+  const complexExample = (author) => {
+    console.log(author);
+  }
   return (
     <article className='book'>
-      <img src={img} alt="coverBook" />
+      <img src={img} alt="coverBook" onMouseOver={() => {
+        console.log(title)
+      }} />
       <h1>{title.toUpperCase()}</h1>
       <h4>{author}</h4>
       {children}
+      <button type="button" onClick={clickHandler}>
+        Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        {/* sẽ click duy nhất 1 đối tượng được chỉ định */}
+        Example
+      </button>
     </article>
   )
 }
